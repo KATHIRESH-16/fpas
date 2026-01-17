a918635e2b8f290396a4a3461de4ecdb21faedfd
KATHIRESH-16 <kathirmak16@gmail.com>
2026-01-17 13:35:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,21 @@
-self.addEventListener("install", e=>self.skipWaiting());
-self.addEventListener("fetch", e=>{});
+{
+  "name": "FPAS College Scheduler",
+  "short_name": "FPAS",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#020617",
+  "theme_color": "#38bdf8",
+  "description": "Faculty Period Alert System - Offline & Downloadable",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
